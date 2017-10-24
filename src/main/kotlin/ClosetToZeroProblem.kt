338942cf1a2c706b98d7e3b6e78d1843c0adb3df
Iskander Ignatev <iskander.ignatev@gmail.com>
2017-10-25 01:09:51 +0400

    enable spek tests

--- src/main/kotlin/ClosetToZeroProblem.kt
+++ src/main/kotlin/ClosetToZeroProblem.kt
@@ -1,7 +1,21 @@
 fun main(args: Array<String>) {
+    val inputString = "aabbddccca"
+    isPalindromable(inputString)
+}
+
+fun isPalindromable(inputString: String): Boolean {
+    println(inputString.toSet())
+
+    return inputString.toSet().filter { c -> inputString.count { it == c } % 2 == 1 }.count() < 2
 
 }
 
-fun arrayChange(inputArray: MutableList<Int>): Int {
-    return 3
+fun areEquallyStrong(yourLeft: Int, yourRight: Int, friendsLeft: Int, friendsRight: Int): Boolean = listOf(yourLeft, yourRight).sorted() == listOf(friendsLeft, friendsRight).sorted()
+
+
+fun arrayMaximalAdjacentDifference(inputArray: MutableList<Int>) = (1 until inputArray.size).map{Math.abs(inputArray[it] - inputArray[it - 1])}.max()
+
+fun isIPv4Address(inputString: String):Boolean {
+    val tokens = inputString.split(".")
+    return tokens.size == 4 && tokens.all{it.toIntOrNull() in 0..255}
 }
--- src/test/kotlin/ArrayMovesSpec.kt
+++ src/test/kotlin/ArrayMovesSpec.kt
@@ -1,3 +1,4 @@
+import org.assertj.core.api.Assertions.`in`
 import org.assertj.core.api.Assertions.assertThat
 import org.jetbrains.spek.api.Spek
 import org.jetbrains.spek.api.dsl.given
@@ -5,12 +6,50 @@ import org.jetbrains.spek.api.dsl.it
 import org.jetbrains.spek.api.dsl.on
 
 object SimpleSpec: Spek({
-    given("array") {
-        val array = mutableListOf(1, 1, 1)
+    given("string") {
+        val inputString = "aabbb"
         on("check") {
-            val result = arrayChange(array)
-            it("should return 3") { assertThat(result).isEqualTo(3) }
+            val result = isPalindromable(inputString)
+            it("should return true") { assertThat(result).isEqualTo(true) }
         }
 
     }
 })
+
+object PairSpec: Spek({
+    given("pairs") {
+        val yourLeft = 15
+        val yourRight = 10
+        val friendsLeft = 10
+        val friendsRight = 15
+        on("check") {
+            val result = areEquallyStrong(yourLeft, yourRight, friendsLeft, friendsRight)
+            it("should return true") { assertThat(result).isEqualTo(true) }
+        }
+    }
+})
+
+object ipv4Spec: Spek({
+    given("ipString") {
+        val ipString = "255.2.3.4"
+        on("check") {
+            val result = isIPv4Address(ipString)
+            it("should return true") {
+                assertThat(result).isEqualTo(true)
+            }
+        }
+    }
+})
+
+
+object DiffSpec: Spek({
+    given("array") {
+        val inputArray = listOf(1, 3, 0, 4)
+        on("check") {
+            val result = arrayMaximalAdjacentDifference(inputArray as MutableList<Int>)
+            it("should return 4") {assertThat(result).isEqualTo(4)}
+        }
+    }
+})
+
+
